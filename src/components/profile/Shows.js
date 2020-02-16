e008f8d1dfaeffb41ae95971629692c14b22c72c
ToastMasterson <tylercmasterson@gmail.com>
2020-02-15 23:40:33 -0700

    Cleans up code

--- src/components/profile/Shows.js
+++ src/components/profile/Shows.js
@@ -1,59 +1,24 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableContainer from '@material-ui/core/TableContainer'
-import TableHead from '@material-ui/core/TableHead'
-import TablePagination from '@material-ui/core/TablePagination'
-import TableRow from '@material-ui/core/TableRow'
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core'
 
 const columns = [
     { id: 'flyer', label: '', minWidth: 170, align: 'center' },
     { id: 'title', label: 'Title', minWidth: 170, align: 'center' },
     { id: 'date', label: 'Date', minWidth: 100, align: 'center' },
-    {
-      id: 'venue',
-      label: 'Venue',
-      minWidth: 170,
-      align: 'center',
-      format: value => value.toLocaleString(),
-    },
-    {
-      id: 'time',
-      label: 'Time',
-      minWidth: 170,
-      align: 'center',
-      format: value => value.toLocaleString(),
-    },
-    // {
-    //   id: 'density',
-    //   label: 'Density',
-    //   minWidth: 170,
-    //   align: 'right',
-    //   format: value => value.toFixed(2),
-    // },
+    { id: 'venue', label: 'Venue', minWidth: 170, align: 'center' },
+    { id: 'time', label: 'Time', minWidth: 170, align: 'center' },
   ]
 
 const useStyles = makeStyles({
-    paper: {
-        width: '100%',
-    },
-    container: {
-        maxHeight: 440,
-    },
-    row: {
-        height: '150px'
-    },
-    img: {
-        width: '100%',
-        maxWidth: '100px'
-    }
+    paper: { width: '100%' },
+    container: { maxHeight: 440 },
+    row: { height: '150px' },
+    img: { width: '100%', maxWidth: '100px' }
 })
 
-function createData(flyer, title, date, venue, time) {
-    return { flyer, title, date, venue, time };
+const createData = (flyer, title, date, venue, time) => {
+    return { flyer, title, date, venue, time }
 }
 
 const rows = [
@@ -63,7 +28,7 @@ const rows = [
     createData('flyer', 'Show 4', '5/25/2020', 'Hi-Dive', '7:00PM'),
     createData('flyer', 'Show 5', '5/25/2020', 'Hi-Dive', '7:00PM'),
     createData('flyer', 'Show 6', '5/25/2020', 'Hi-Dive', '7:00PM'),
-    createData('flyer', 'Show 7', '5/25/2020', 'Hi-Dive', '7:00PM'),
+    createData('flyer', 'Show 7', '5/25/2020', 'Hi-Dive', '7:00PM')
 ]
 
 const Shows = () => {
@@ -75,54 +40,42 @@ const Shows = () => {
             <Paper className={classes.paper}>
                 <TableContainer className={classes.container}>
                     <Table stickyHeader aria-label="sticky table">
-                    <TableHead>
-                        <TableRow>
-                        {columns.map(column => (
-                            <TableCell
-                            key={column.id}
-                            align={column.align}
-                            style={{ minWidth: column.minWidth }}
-                            >
-                            {column.label}
-                            </TableCell>
-                        ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map(row => {
-                        return (
-                            <TableRow className={classes.row} hover role="checkbox" tabIndex={-1} key={row.title}>
-                                {/* <TableCell>
-
-                                </TableCell> */}
-                                {columns.map(column => {
-                                    const value = row[column.id];
-                                    return (
-                                        column.id === 'flyer'
-                                            ? <TableCell key={column.id} align={column.align}>
-                                                    <img className={classes.img} src={value} alt="flyer" />
-                                            </TableCell>
-                                            : <TableCell key={column.id} align={column.align}>
-                                                {column.format && typeof value === 'number' ? column.format(value) : value}
-                                            </TableCell>
-                                    );
-                                })}
+                        <TableHead>
+                            <TableRow>
+                            {columns.map(column => (
+                                <TableCell
+                                    key={column.id}
+                                    align={column.align}
+                                    style={{ minWidth: column.minWidth }}
+                                >
+                                    {column.label}
+                                </TableCell>
+                            ))}
                             </TableRow>
-                        );
-                        })}
-                    </TableBody>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map(row => {
+                                return (
+                                    <TableRow className={classes.row} hover role="checkbox" tabIndex={-1} key={row.title}>
+                                        {columns.map(column => {
+                                            const value = row[column.id];
+                                            return (
+                                                column.id === 'flyer'
+                                                    ? <TableCell key={column.id} align={column.align}>
+                                                            <img className={classes.img} src={value} alt="flyer" />
+                                                    </TableCell>
+                                                    : <TableCell key={column.id} align={column.align}>
+                                                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                                                    </TableCell>
+                                            )
+                                        })}
+                                    </TableRow>
+                                )
+                            })}
+                        </TableBody>
                     </Table>
                 </TableContainer>
-                {/* <TablePagination
-                    rowsPerPageOptions={[10, 25, 100]}
-                    component="div"
-                    count={rows.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onChangePage={handleChangePage}
-                    onChangeRowsPerPage={handleChangeRowsPerPage}
-                /> */}
-                </Paper>
+            </Paper>
         </div>
     )
 }
