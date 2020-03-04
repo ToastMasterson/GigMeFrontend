ffefe384e7ed1ea3ba430515566f0046dae5c89d
ToastMasterson <tylercmasterson@gmail.com>
2020-03-04 15:26:40 -0700

    Organizes NavBar Code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -20,7 +20,7 @@ import ListItemText from '@material-ui/core/ListItemText'
 import ChatIcon from '@material-ui/icons/Chat'
 import EventIcon from '@material-ui/icons/Event'
 import PersonAddIcon from '@material-ui/icons/PersonAdd'
-import {Link as NavLink, Container, Grid, Hidden, Divider} from '@material-ui/core' 
+import {Link as NavLink, Grid, Hidden, Divider} from '@material-ui/core' 
 
 
 import '../style_sheets/NavBar.css'
@@ -106,13 +106,12 @@ const useStyles = makeStyles(theme => ({
 function ElevationScroll(props) {
     const { children, window } = props
     const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 0,
-    target: window ? window() : undefined,
+      disableHysteresis: true,
+      threshold: 0,
+      target: window ? window() : undefined,
     })
-
     return React.cloneElement(children, {
-    elevation: trigger ? 4 : 0,
+      elevation: trigger ? 4 : 0,
     })
 }
 
@@ -165,38 +164,38 @@ export default function PrimarySearchAppBar(props) {
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
-        <Link className={classes.menuLink} to="/profile">
-            <StyledMenuItem onClick={handleMenuClose}>
-                <ListItemIcon>
-                    <AccountCircle fontSize="small" />
-                </ListItemIcon>
-                <ListItemText primary="Profile" />
-            </StyledMenuItem>
-        </Link>
-        <Link className={classes.menuLink} to="/settings">
-            <StyledMenuItem onClick={handleMenuClose}>
-                <ListItemIcon>
-                    <SettingsIcon fontSize="small" />
-                </ListItemIcon>
-                <ListItemText primary="Account" />
-            </StyledMenuItem>
-        </Link>
-        <Link className={classes.menuLink} to="/requests">
-            <StyledMenuItem onClick={handleMenuClose}>
-                <ListItemIcon>
-                    <PersonAddIcon />
-                </ListItemIcon>
-                <ListItemText primary="Requests" />
-            </StyledMenuItem>
-        </Link>
-        <Link className={classes.menuLink} to="/calendar">
-            <StyledMenuItem onClick={handleMenuClose}>
-                <ListItemIcon>
-                    <EventIcon />
-                </ListItemIcon>
-                <ListItemText primary="Calendar" />
-            </StyledMenuItem>
-        </Link>
+      <Link className={classes.menuLink} to="/profile">
+        <StyledMenuItem onClick={handleMenuClose}>
+          <ListItemIcon>
+              <AccountCircle fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Profile" />
+        </StyledMenuItem>
+      </Link>
+      <Link className={classes.menuLink} to="/settings">
+        <StyledMenuItem onClick={handleMenuClose}>
+          <ListItemIcon>
+              <SettingsIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Account" />
+        </StyledMenuItem>
+      </Link>
+      <Link className={classes.menuLink} to="/requests">
+        <StyledMenuItem onClick={handleMenuClose}>
+          <ListItemIcon>
+              <PersonAddIcon />
+          </ListItemIcon>
+          <ListItemText primary="Requests" />
+        </StyledMenuItem>
+      </Link>
+      <Link className={classes.menuLink} to="/calendar">
+        <StyledMenuItem onClick={handleMenuClose}>
+          <ListItemIcon>
+              <EventIcon />
+          </ListItemIcon>
+          <ListItemText primary="Calendar" />
+        </StyledMenuItem>
+      </Link>
     </Menu>
   )
 
@@ -212,54 +211,54 @@ export default function PrimarySearchAppBar(props) {
       onClose={handleMobileMenuClose}
     >
       <Link className={classes.menuLink} to="/profile">
-            <StyledMenuItem onClick={handleMenuClose}>
-                <ListItemIcon>
-                    <AccountCircle fontSize="small" />
-                </ListItemIcon>
-                <ListItemText primary="Profile" />
-            </StyledMenuItem>
-        </Link>
-        <Link className={classes.menuLink} to="/messages">
-            <StyledMenuItem onClick={handleMenuClose}>
-                <ListItemIcon>
-                    <ChatIcon />
-                </ListItemIcon>
-                <ListItemText primary="Messages" />
-            </StyledMenuItem>
-        </Link>
-        <Link className={classes.menuLink} to="/requests">
-            <StyledMenuItem onClick={handleMenuClose}>
-                <ListItemIcon>
-                    <NotificationsIcon />
-                </ListItemIcon>
-                <ListItemText primary="Requests" />
-            </StyledMenuItem>
-        </Link>
-        <Divider />
-        <Link className={classes.menuLink} to="/settings">
-            <StyledMenuItem onClick={handleMenuClose}>
-                <ListItemIcon>
-                    <SettingsIcon fontSize="small" />
-                </ListItemIcon>
-                <ListItemText primary="Account" />
-            </StyledMenuItem>
-        </Link>
-        <Link className={classes.menuLink} to="/requests">
-            <StyledMenuItem onClick={handleMenuClose}>
-                <ListItemIcon>
-                    <PersonAddIcon />
-                </ListItemIcon>
-                <ListItemText primary="Requests" />
-            </StyledMenuItem>
-        </Link>
-        <Link className={classes.menuLink} to="/calendar">
-            <StyledMenuItem onClick={handleMenuClose}>
-                <ListItemIcon>
-                    <EventIcon />
-                </ListItemIcon>
-                <ListItemText primary="Calendar" />
-            </StyledMenuItem>
-        </Link>
+        <StyledMenuItem onClick={handleMenuClose}>
+          <ListItemIcon>
+              <AccountCircle fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Profile" />
+        </StyledMenuItem>
+      </Link>
+      <Link className={classes.menuLink} to="/messages">
+        <StyledMenuItem onClick={handleMenuClose}>
+            <ListItemIcon>
+                <ChatIcon />
+            </ListItemIcon>
+            <ListItemText primary="Messages" />
+        </StyledMenuItem>
+      </Link>
+      <Link className={classes.menuLink} to="/requests">
+        <StyledMenuItem onClick={handleMenuClose}>
+          <ListItemIcon>
+              <NotificationsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Requests" />
+        </StyledMenuItem>
+      </Link>
+      <Divider />
+      <Link className={classes.menuLink} to="/settings">
+        <StyledMenuItem onClick={handleMenuClose}>
+          <ListItemIcon>
+              <SettingsIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Account" />
+        </StyledMenuItem>
+      </Link>
+      <Link className={classes.menuLink} to="/requests">
+        <StyledMenuItem onClick={handleMenuClose}>
+          <ListItemIcon>
+              <PersonAddIcon />
+          </ListItemIcon>
+          <ListItemText primary="Requests" />
+        </StyledMenuItem>
+      </Link>
+      <Link className={classes.menuLink} to="/calendar">
+        <StyledMenuItem onClick={handleMenuClose}>
+          <ListItemIcon>
+              <EventIcon />
+          </ListItemIcon>
+          <ListItemText primary="Calendar" />
+        </StyledMenuItem>
+      </Link>
     </Menu>
   )
 
@@ -267,75 +266,75 @@ export default function PrimarySearchAppBar(props) {
     <div className={classes.grow}>
         <CssBaseLine />
         <ElevationScroll {...props}>
-            <AppBar position="static" style={{ color: '#5497c7', backgroundColor: '#2a2a2d'}}>
-                <Toolbar>
-                <Typography className={classes.title}>
-                    <Link to="/">
-                        <img className="navbar-logo" src="https://i.imgur.com/mQWWPgC.png" alt="main-logo"/>
-                    </Link>
-                </Typography>
-                <div className={classes.search}>
-                    <div className={classes.searchIcon}>
-                    <SearchIcon />
-                    </div>
-                    <InputBase
-                    placeholder="Search…"
-                    classes={{
-                        root: classes.inputRoot,
-                        input: classes.inputInput,
-                    }}
-                    inputProps={{ 'aria-label': 'search' }}
-                    />
+          <AppBar position="static" style={{ color: '#5497c7', backgroundColor: '#2a2a2d'}}>
+            <Toolbar>
+              <Typography className={classes.title}>
+                <Link to="/">
+                  <img className="navbar-logo" src="https://i.imgur.com/mQWWPgC.png" alt="main-logo"/>
+                </Link>
+              </Typography>
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
                 </div>
-                <Hidden xsDown>
-                  <Grid container spacing={2}>
-                    <Grid item>
-                      <NavLink className={classes.link} component={Link} to="/dashboard" >Dashboard</NavLink>
-                    </Grid>
-                    <Grid item>
-                      <NavLink className={classes.link} component={Link} to="/discover_artists">Artists</NavLink>
-                    </Grid>
-                    <Grid item>
-                      <NavLink className={classes.link}>Venues</NavLink>
-                    </Grid>
+                <InputBase
+                  placeholder="Search…"
+                  classes={{
+                      root: classes.inputRoot,
+                      input: classes.inputInput,
+                  }}
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+              </div>
+              <Hidden xsDown>
+                <Grid container spacing={2}>
+                  <Grid item>
+                    <NavLink className={classes.link} component={Link} to="/dashboard" >Dashboard</NavLink>
                   </Grid>
-                </Hidden>
-                <div className={classes.grow} />
-                <div className={classes.sectionDesktop}>
-                    <IconButton aria-label="show 4 new mails" color="inherit">
-                    <Badge badgeContent={0} color="secondary">
-                        <ChatIcon />
-                    </Badge>
-                    </IconButton>
-                    <IconButton aria-label="show 17 new notifications" color="inherit">
-                    <Badge badgeContent={0} color="secondary">
-                        <NotificationsIcon />
-                    </Badge>
-                    </IconButton>
-                    <IconButton
-                    edge="end"
-                    aria-label="account of current user"
-                    aria-controls={menuId}
-                    aria-haspopup="true"
-                    onClick={handleProfileMenuOpen}
-                    color="inherit"
-                    >
-                    <AccountCircle />
-                    </IconButton>
-                </div>
-                <div className={classes.sectionMobile}>
-                    <IconButton
-                    aria-label="show more"
-                    aria-controls={mobileMenuId}
-                    aria-haspopup="true"
-                    onClick={handleMobileMenuOpen}
-                    color="inherit"
-                    >
-                    <MoreIcon />
-                    </IconButton>
-                </div>
-                </Toolbar>
-            </AppBar>
+                  <Grid item>
+                    <NavLink className={classes.link} component={Link} to="/discover_artists">Artists</NavLink>
+                  </Grid>
+                  <Grid item>
+                    <NavLink className={classes.link}>Venues</NavLink>
+                  </Grid>
+                </Grid>
+              </Hidden>
+              <div className={classes.grow} />
+              <div className={classes.sectionDesktop}>
+                <IconButton aria-label="show 4 new mails" color="inherit">
+                  <Badge badgeContent={0} color="secondary">
+                      <ChatIcon />
+                  </Badge>
+                </IconButton>
+                <IconButton aria-label="show 17 new notifications" color="inherit">
+                  <Badge badgeContent={0} color="secondary">
+                    <NotificationsIcon />
+                  </Badge>
+                </IconButton>
+                <IconButton
+                  edge="end"
+                  aria-label="account of current user"
+                  aria-controls={menuId}
+                  aria-haspopup="true"
+                  onClick={handleProfileMenuOpen}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+              </div>
+              <div className={classes.sectionMobile}>
+                <IconButton
+                  aria-label="show more"
+                  aria-controls={mobileMenuId}
+                  aria-haspopup="true"
+                  onClick={handleMobileMenuOpen}
+                  color="inherit"
+                >
+                  <MoreIcon />
+                </IconButton>
+              </div>
+            </Toolbar>
+          </AppBar>
         </ElevationScroll>
         {renderMobileMenu}
         {renderMenu}
