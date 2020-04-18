8ca39646c227e0df3a7835021903a0bf484dead5
ToastMasterson <tylercmasterson@gmail.com>
2020-04-18 13:19:51 -0600

    Adds TabPaanel Component to Landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import firebase, { auth, provider } from '../firebase.js'
+import PropTypes from 'prop-types'
 import CssBaseLine from '@material-ui/core/CssBaseline'
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
@@ -6,7 +8,7 @@ import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 import Divider from '@material-ui/core/Divider'
 import Button from '@material-ui/core/Button'
-import { makeStyles } from '@material-ui/core'
+import { makeStyles, Box, Tab, Tabs } from '@material-ui/core'
 
 
 const useStyles = makeStyles(theme => ({
@@ -66,9 +68,69 @@ const useStyles = makeStyles(theme => ({
     }
 }))
 
+const TabPanel = (props) => {
+    const { children, value, index, ...other } = props
+    return (
+        <Typography
+            component="div"
+            role="tabpanel"
+            hidden={value !== index}
+            id={`nav-tabpanel-${index}`}
+            aria-labelledby={`nav-tab-${index}`}
+            { ...other }
+        >
+            {value === index && <Box p={3}>{children}</Box>}
+        </Typography>
+    )
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+}
+
+const a11yProps = (index) => {
+    return {
+        id: `nav-tab-${index}`,
+        'aria-controls': `nav-tabpanel-${index}`,
+    }
+}
+
+const LinkTab = (props) => {
+    return (
+        <Tab
+            component="a"
+            onClick={(event) => {
+                event.preventDefault()
+            }}
+            { ...props }
+        />
+    )
+}
+
 const Landing = () => {
 
     const classes = useStyles()
+    const [state, setState] = React.useState({
+        user: null,
+        value: 0
+    })
+
+    const handleChange = ( event, newValue ) => {
+        setState({ value: newValue })
+    }
+    const gLogIn = () => {
+        auth.signInWithPopup(provider)
+        .then((result) => {
+            const user = result.user
+            setState({ user })
+        })
+    }
+
+    const signUp = () => {
+
+    }
 
     return (
         <div className="landing">
@@ -86,16 +148,22 @@ const Landing = () => {
                             </Grid>
                             <Divider className={classes.divider} variant="fullWidth"/>
                             <Grid className={classes.gridItem} item xs container justify="center" spacing={2}>
-                                <Grid item xs={6}>
-                                    <Button className={classes.button} variant="contained">
+                                <Paper>
+                                    <Tabs
+                                        variant="fullWidth"
+                                        value={state.value}
+                                        onChange={handleChange}
+                                    >
+                                        <LinkTab label="Log In" href="/login" { ...a11yProps(0)} />
+                                        <LinkTab label="Sign Up" href="/signup" { ...a11yProps(1)} />
+                                    </Tabs>
+                                    <TabPanel value={state.value} index={0}>
                                         Log In
-                                    </Button>
-                                </Grid>
-                                <Grid item xs={6}>
-                                    <Button className={classes.button} variant="contained">
+                                    </TabPanel>
+                                    <TabPanel value={state.value} index={1}>
                                         Sign Up
-                                    </Button>
-                                </Grid>
+                                    </TabPanel>
+                                </Paper>
                             </Grid>
                         </Grid>
                     </Paper>
